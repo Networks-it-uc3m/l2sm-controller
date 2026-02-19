29be39416717a283e264c095143dd22134f10363
Tjaarda1 <abuning@pa.uc3m.es>
2026-02-18 21:59:19 -0800

    feature: added del port cli

---
+++ apps/vnets/src/main/java/org/l2sm/vnets/cli/L2SMDelPort.java
@@ -0,0 +1,34 @@
+package org.l2sm.vnets.cli;
+
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.l2sm.vnets.api.IDCOService;
+import org.l2sm.vnets.api.IDCOServiceException;
+import org.onosproject.cli.AbstractShellCommand;
+import org.onosproject.net.ConnectPoint;
+
+@Service
+@Command(scope = "onos", name = "l2sm-del-port", description = "Add a port to an existing network")
+
+public class L2SMDelPort extends AbstractShellCommand {
+
+    @Argument(index = 0, name = "networkId", description = "networkId", required = true, multiValued = false)
+    String networkId = null;
+
+    @Argument(index = 1, name = "networkEndpoint", description = "networkEndpoint", required = true, multiValued = false)
+    String networkEndpoint = null;
+
+    @Override
+    protected void doExecute() {
+        IDCOService idcoService = get(IDCOService.class);
+        try {
+            idcoService.deletePort(networkId, ConnectPoint.deviceConnectPoint(networkEndpoint));
+            print("Port " + networkEndpoint + " added to network " + networkId + " successfully.");
+        } catch (IDCOServiceException e) {
+            print("Error adding port to network: " + e.getMessage());
+        } catch (Exception e) {
+            print("Unexpected error occurred: " + e.toString());
+        }
+    }
+}
