712fd4967bf7e4e92343db9102a8f40753e4b444
Tjaarda1 <abuning@pa.uc3m.es>
2026-03-30 07:22:18 -0700

    added correct license header

--- apps/vlinks/src/main/java/org/l2sm/vlinks/package-info.java
+++ apps/vlinks/src/main/java/org/l2sm/vlinks/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024-present Open Networking Foundation
+ * Copyright 2026 Universidad Carlos III de Madrid
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
