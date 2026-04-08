2120c89ab1a5255ce1c31882d71e726956159b52
Tjaarda1 <abuning@pa.uc3m.es>
2026-04-08 03:51:30 -0700

    feature(mirroring): added cli for testing the mirrored ports in the network

--- apps/vnets/src/main/java/org/l2sm/vnets/cli/L2SMCreateNetwork.java
+++ apps/vnets/src/main/java/org/l2sm/vnets/cli/L2SMCreateNetwork.java
@@ -2,10 +2,12 @@ package org.l2sm.vnets.cli;
 
 import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.l2sm.vnets.api.IDCOService;
 import org.l2sm.vnets.api.IDCOServiceException;
 import org.onosproject.cli.AbstractShellCommand;
+import org.onosproject.net.ConnectPoint;
 
 @Service
 @Command(scope = "onos", name = "l2sm-create-network", description = "Create a network")
@@ -15,11 +17,16 @@ public class L2SMCreateNetwork extends AbstractShellCommand {
     @Argument(index = 0, name = "networkId", description = "networkId", required = true, multiValued = false)
     String networkId = null;
 
+    @Option(name = "-m", aliases = "--mirror-port", description = "mirror port connect point", required = false,
+            multiValued = false)
+    String mirrorPort = null;
+
     @Override
     protected void doExecute() {
         IDCOService idcoService = get(IDCOService.class);
         try {
-            idcoService.createVirtualNetwork(networkId);
+            ConnectPoint parsedMirrorPort = mirrorPort == null ? null : ConnectPoint.deviceConnectPoint(mirrorPort);
+            idcoService.createVirtualNetwork(networkId, parsedMirrorPort);
             print("Network " + networkId + " created successfully.");
         } catch (IDCOServiceException e) {
             print("Error creating network: " + e.getMessage());
